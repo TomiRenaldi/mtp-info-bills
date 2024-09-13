847a722caefde3bf299a3cb9bd67a06a44c5e87c
TomiRenaldi <tomirenaldi2@gmail.com>
2024-09-13 15:35:59 +0700

    Jadi tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminDashboard from './pages/AdminDashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => {
   return (
     <Router>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Routes>
         <Route path="/" element={<AdminDashboard />} />
       </Routes>
